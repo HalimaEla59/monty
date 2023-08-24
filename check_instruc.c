7ac222bfa5bcd0a875371cb4509f4531e2a4ad1a
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-08-24 15:30:30 +0100

    check_instruc.c added

---
+++ check_instruc.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+
+/**
+ * check_instruc - checks for and returns the right instruction
+ * @line: the line that has instruction
+ * Return: pointer to the instruction
+ */
+
+instruction_t *check_instruc(char *line)
+{
+	char *opcode;
+	instruction_t *instruction;
+
+	opcode = get_opcode(line);
+	instruction = malloc(sizeof(*instruction));
+	if (instruction == NULL)
+	{
+		fprintf(stdout, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	instruction->opcode = opcode;
+	instruction->f = NULL;
+	if (instruction->opcode)
+	{
+		if (strcmp(instruction->opcode, "push") == 0)
+			instruction->f = push;
+		if (strcmp(instruction->opcode, "pall") == 0)
+			instruction->f = pall;
+		if (strcmp(instruction->opcode, "pint") == 0)
+			instruction->f = pint;
+		if (strcmp(instruction->opcode, "pop") == 0)
+			instruction->f = pop;
+		if (strcmp(instruction->opcode, "swap") == 0)
+			instruction->f = swap;
+		if (strcmp(instruction->opcode, "add") == 0)
+			instruction->f = add;
+		if (strcmp(instruction->opcode, "nop") == 0)
+			instruction->f = nop;
+		if (strcmp(instruction->opcode, "stack") == 0)
+			instruction->f = stack;
+		if (strcmp(instruction->opcode, "queue") == 0)
+			instruction->f = queue;
+	}
+	return (instruction);
+}
